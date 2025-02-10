c1e791a216d307f29ff7ba5b109249ab5f14e970
josegcpa <jose.almeida@research.fchampalimaud.org>
2025-02-10 01:11:17 +0000

    filter files for nnunet inference

---
+++ scripts/link-for-segmentation.sh
@@ -0,0 +1,17 @@
+IN_DIR=$1
+OUT_DIR=$2
+
+mkdir -p $OUT_DIR
+
+declare -A SERIES_TO_NUM
+SERIES_TO_NUM[T2]="0000"
+SERIES_TO_NUM[HBV]="0001"
+SERIES_TO_NUM[ADC]="0002"
+
+for series_type in $SEGMENTATION_SERIES
+do
+    for file in $IN_DIR/*${SERIES_TO_NUM[$series_type]}*
+    do
+        cp $file $OUT_DIR/$(basename $file)
+    done
+done
